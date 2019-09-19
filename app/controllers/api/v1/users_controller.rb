76839ded29566ff64d670bdd2e2de0103ad4aab9
Josh Pigford <josh@joshpigford.com>
2019-09-19 14:37:05 -0500

    Adds update action to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,9 +13,21 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :password)    
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
